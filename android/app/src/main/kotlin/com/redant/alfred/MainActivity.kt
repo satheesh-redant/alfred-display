1986a700ebdfe42a4da9e1fb4aa0855fa568bd0a
Satheesh Kumar Pingali <satheesh.kumar@redanttechsys.com>
2025-05-29 05:08:05 +0000

    Merged in satheesh (pull request #4)
    feat: Update my_application.cc
    
    * feat: Update my_application.cc
    - Makes the window fullscreen and removes window decorations.
    refactor: Removed Android platform

--- android/app/src/main/kotlin/com/redant/alfred/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.redant.alfred
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {}
-
--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -50,6 +50,9 @@ static void my_application_activate(GApplication* application) {
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
+  gtk_window_fullscreen(GTK_WINDOW(window));       // Makes the window fullscreen
+  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);  // Removes window
+
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
